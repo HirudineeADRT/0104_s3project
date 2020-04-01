df9ac6437d6a75b3567cfe6399badeed35b4e44a
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-01 11:50:55 +0530

    sqs added

---
+++ 0104_SQSproject.js
@@ -0,0 +1,57 @@
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
+exports.handler = async (event) => {
+
+    try {
+        let data = await sqs.sendMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hiru-test`,
+            MessageBody: "test sample message",
+            DelaySeconds: 0,
+            MessageAttributes: {
+                '1': {
+                    'DataType': "String",
+                    'StringValue': "test"
+                }
+            }
+        }).promise();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+        // error handling goes here
+    };
+
+
+
+    try {
+        let data = await sqs.receiveMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hiru-test`,
+            MaxNumberOfMessages: 1,
+            VisibilityTimeout: 30,
+            WaitTimeSeconds: 0,
+            AttributeNames: ['All']
+        }).promise();
+        console.log(data);
+    handle = data.Messages[0].ReceiptHandle;
+    console.log(handle);
+    try {
+        let data = await sqs.deleteMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hiru-test`,
+            ReceiptHandle: handle
+        }).promise();
+        console.log(data);
+    } catch (err) {
+        console.log(err)
+        // error handling goes here
+    };
+
+    } catch (err) {
+        console.log(err);
+        // error handling goes here
+    };
+
+
+
+
+    return { "message": "Successfully executed" };
+};
